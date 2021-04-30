50c73f74327e431e07bf9a425d7725f8fce7aa2d
Balazs Orehovszki <orehovszki.b@gmail.com>
2021-05-01 01:45:56 +0200

    - Added function to read file to String - Added error message if missing file couldn't be created

--- src/main/java/thorxs/Utils.java
+++ src/main/java/thorxs/Utils.java
@@ -7,10 +7,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Utils {
-    /*
-    * TODO: Make sure the file reading and writing can be done inside a jar to make the application more compact (resource access)
-    */
-
     /**
      * Writes data into a CSV file
      * @param fileName Path and name of the file
@@ -29,14 +25,14 @@ public class Utils {
             writer.flush();
             writer.close();
         } catch (IOException e) {
-            JOptionPane.showMessageDialog(new JFrame(), "Error while writing file:\nMessage: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(new JDialog(), "Error while writing file:\nMessage: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
     /**
      * Reads a CSV file and returns its content
      * @param fileName Path and name of the file
-     * @return A string list
+     * @return A String list
      */
     public static List<String[]> readCSV(String fileName) {
         List<String[]> data = new ArrayList<>();
@@ -55,17 +51,50 @@ public class Utils {
 
                 br.close();
             } catch (IOException e) {
-                JOptionPane.showMessageDialog(new JFrame(), "Error while reading file:\n" + fileName + "Message: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(new JDialog(), "Error while reading file:\n" + fileName + "Message: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
             }
         } else {
-            JOptionPane.showMessageDialog(new JFrame(), "Error: File not found!", "Error", JOptionPane.ERROR_MESSAGE);
+            // If the file is not found, create it
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(new JDialog(), "Error: File: " + fileName + "could not be created!", "Error", JOptionPane.ERROR_MESSAGE);
+            }
         }
 
         // Check if any data was read
         if (data.isEmpty()) {
-            JOptionPane.showMessageDialog(new JFrame(), "Warning: No data was read from file:\n" + fileName, "Warning", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(new JDialog(), "Warning: No data was read from file:\n" + fileName, "Warning", JOptionPane.WARNING_MESSAGE);
         }
 
         return data;
+    }
+
+    /**
+     * Reads a file into a String
+     * @param fileName Path and name of the file
+     * @return A String
+     */
+    public static String readFileToString(String fileName) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            StringBuilder sb = new StringBuilder();
+
+            String line = null;
+            String ls = System.getProperty("line.separator");
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+                sb.append(ls);
+            }
+
+            sb.deleteCharAt(sb.length() - 1);
+            reader.close();
+
+            return sb.toString();
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(new JDialog(), "Error while reading file to string:\n" + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
+        return null;
     }
 }
